39c96dccc46863e36928f0128885aa5c0324bfe5
FOUQUET aurelie <aurfouquet@gmail.com>
2022-01-04 13:35:55 +0100

    factorisation finie

--- include/cassini.h
+++ include/cassini.h
@@ -37,8 +37,17 @@ void write_op_to_pipe(uint16_t operation, char* path_request, char *path_reply);
 //prend une operation et un taskid et les ecrit dans le pipe request
 void write_op_taskid_to_pipe(uint16_t operation, uint64_t taskid, char* path_request, char *path_reply);
 
+//ecriture pour la commande CREATE
+void write_create(uint16_t operation, int argc, char * argv[], char * minutes_str, char * hours_str, char * daysofweek_str, char *path_request, char *path_reply);
+  
 //lecture pour les options e et o (reptype OK ou ERROR)
 void read_reptype_e_o(char *path_reply, char *path_request);
+
+//lecture pour la commande LIST
+void read_list(char *path_reply, char *path_request);
+
+//lecture pour la commande TIMES_EXITCODE
+void read_times_exitcode(char *path_reply, char *path_request);
 
 //renvoie le reptype lu
 uint16_t read_reptype(int fd_reply, char *path_reply, char *path_request);
--- src/cassini.c
+++ src/cassini.c
@@ -49,6 +49,48 @@ void write_op_taskid_to_pipe(uint16_t operation, uint64_t taskid, char* path_req
 }
 
 
+//ecriture pour la commande CREATE
+void write_create(uint16_t operation, int argc, char * argv[], char * minutes_str, char * hours_str, char * daysofweek_str, char *path_request, char *path_reply) {
+  int fd_request = open(path_request, O_WRONLY);
+  if (fd_request == -1) { free_and_exit(path_request, path_reply); }
+  char buffer[1024];
+  
+  //ecriture de l'opcode
+  int buffer_size = sizeof(uint16_t);
+  uint16_t opcode = htobe16(operation);
+  memmove(buffer,&opcode,sizeof(uint16_t));
+  
+  //ecriture du timing (MINUTES <uint64>, HOURS <uint32>, DAYSOFWEEK <uint8>)
+  struct timing t;
+  int n = timing_from_strings(&t, minutes_str, hours_str, daysofweek_str);
+  if (n==-1) { free_and_exit(path_request, path_reply); }
+  t.minutes = htobe64(t.minutes);
+  t.hours = htobe32(t.hours);
+  memmove(buffer+buffer_size, &t, sizeof(uint64_t)+sizeof(uint32_t)+sizeof(uint8_t));
+  buffer_size += sizeof(uint64_t)+sizeof(uint32_t)+sizeof(uint8_t);
+  
+  //ecriture de la commandline (ARGC <uint32>, ARGV[0] <string>, ..., ARGV[ARGC-1] <string>)
+  if(argc<1) { free_and_exit(path_request, path_reply); }
+  if(argv[0]==NULL) { free_and_exit(path_request, path_reply); }
+  //ecriture argc
+  uint32_t cmdArgc = htobe32(argc-optind);
+  memmove(buffer+buffer_size, &cmdArgc, sizeof(uint32_t));
+  buffer_size += sizeof(uint32_t);
+  //ecriture argv
+  for (int i = optind; i < argc; i++) {
+    uint32_t length = strlen(argv[i]);
+    uint32_t h = htobe32(length);
+    memmove(buffer+buffer_size, &h, sizeof(uint32_t));
+    char *bufferCmd = argv[i];
+    memmove(buffer+buffer_size+sizeof(uint32_t), bufferCmd, length);
+    buffer_size += sizeof(uint32_t)+length;
+  }
+  int err = write(fd_request, buffer, buffer_size);
+  if (err == -1) { free_and_exit(path_request, path_reply); }
+  close(fd_request);
+}
+
+
 //renvoie le reptype lu, ou -1
 uint16_t read_reptype(int fd_reply, char *path_reply, char *path_request) {
   uint16_t reptype;
@@ -105,6 +147,109 @@ void read_reptype_e_o(char *path_reply, char *path_request) {
 }
 
 
+//lecture pour la commande LIST
+void read_list(char *path_reply, char *path_request) {
+  uint32_t nbtasks;
+  uint32_t cmdArgc;
+  uint64_t taskid;
+
+  int fd_reply = open(path_reply, O_RDONLY);
+  if (fd_reply == -1) { free_and_exit(path_request, path_reply); } 
+  //lecture du reptype
+  uint16_t reptype = read_reptype(fd_reply, path_reply, path_request);
+  if (reptype != SERVER_REPLY_OK) { free_and_exit(path_request, path_reply); }
+  //lecture du nombre de tasks
+  int err = read(fd_reply, &nbtasks, sizeof(uint32_t));
+  if (err == -1) { free_and_exit(path_request, path_reply); }
+  nbtasks = be32toh(nbtasks);
+  if (nbtasks != 0) {
+    //pour chaque task
+    for (int i = 0; i < nbtasks; i++) {
+      //lecture du taskid
+      err = read(fd_reply, &taskid, sizeof(uint64_t));
+      if (err == -1) { free_and_exit(path_request, path_reply); }
+      taskid = be64toh(taskid);
+      printf("%" PRId64 ": ", taskid);
+
+      //lecture du timing
+      timing *t = malloc(sizeof(timing));
+      if (t == NULL) { free_and_exit(path_request, path_reply); }
+      err = read(fd_reply, t, sizeof(uint64_t)+sizeof(uint32_t)+sizeof(uint8_t));
+      if (err == -1) { free_and_exit(path_request, path_reply); }
+      t->minutes = be64toh(t->minutes);
+      t->hours = be32toh(t->hours);
+      char *bufferTiming = malloc(TIMING_TEXT_MIN_BUFFERSIZE);
+      if (bufferTiming == NULL) { free_and_exit(path_request, path_reply); }
+      timing_string_from_timing(bufferTiming, t);
+      printf("%s ", bufferTiming);
+
+      //lecture de la commandline
+      err = read(fd_reply, &cmdArgc, sizeof(uint32_t)); //lecture du nombre d'argv pour la task[i]
+      if (err == -1) { free_and_exit(path_request, path_reply); }
+      cmdArgc = be32toh(cmdArgc);
+      for (int j = 0; j < cmdArgc; j++) {
+        uint32_t stringL;
+        err = read(fd_reply, &stringL, sizeof(uint32_t)); //lecture de la longueur de l'argv[i]
+        if (err == -1) { free_and_exit(path_request, path_reply); }
+        stringL = be32toh(stringL);
+        char bufferCmd[stringL+1];
+        err = read(fd_reply, bufferCmd, stringL); //lecture du contenu de l'argv[i]
+        if (err == -1) { free_and_exit(path_request, path_reply); }
+        bufferCmd[stringL] = '\0';
+        printf("%s ", bufferCmd);
+      }
+      free(t);
+      free(bufferTiming);
+      printf("\n");
+    }    
+  }
+  close(fd_reply);
+}
+
+
+//lecture pour la commande TIMES_EXITCODE
+void read_times_exitcode(char *path_reply, char *path_request) {
+  int fd_reply = open(path_reply, O_RDONLY);
+  if (fd_reply == -1) { free_and_exit(path_request, path_reply); }
+  //lecture du reptype
+  uint16_t reptype = read_reptype(fd_reply, path_reply, path_request);
+  //lecture pour reptype OK
+  if (reptype == SERVER_REPLY_OK) {
+    //lecture du nombre de runs
+    uint32_t nbruns;
+    int err = read(fd_reply, &nbruns, sizeof(uint32_t));
+    if (err == -1) { free_and_exit(path_request, path_reply); }
+    nbruns = be32toh(nbruns);
+    for (int i=0; i<nbruns; i++) { //pour chaque run
+      //lecture de la date et heure du run
+      int64_t time;
+      err = read(fd_reply, &time, sizeof(int64_t));
+      if (err == -1) { free_and_exit(path_request, path_reply); }
+      time = be64toh((uint64_t)time);
+      struct tm * t = localtime(&time);
+      char strBuffer[80];
+      strftime(strBuffer, 80, "%F %T", t);
+      printf("%s ", strBuffer);
+      //lecture de l'exitcode du run
+      uint16_t exitcode;
+      err = read(fd_reply, &exitcode, sizeof(uint16_t));
+      if (err == -1) { free_and_exit(path_request, path_reply); }
+      exitcode = be16toh(exitcode);
+      printf("%u\n", (unsigned int)exitcode);
+    }
+  //lecture pour reptype ERROR
+  } else if (reptype == SERVER_REPLY_ERROR) {
+    //lecture de l'errcode
+    uint16_t errcode = read_errcode(fd_reply, path_reply, path_request);
+    if (errcode == SERVER_REPLY_ERROR_NOT_FOUND) {
+      printf("il n'existe aucune tâche avec cet identifiant");
+      free_and_exit(path_request, path_reply);
+    }
+  }
+  close(fd_reply);
+}
+
+
 //free les espaces memoire alloues et return EXIT_FAILURE
 int free_and_exit(char *path_request, char *path_reply) {
   free(path_request);
@@ -118,7 +263,6 @@ int free_and_exit(char *path_request, char *path_reply) {
 int main(int argc, char * argv[]) {
   errno = 0;
 
-  int fd_request;
   int fd_reply;
   int err;
 
@@ -128,10 +272,7 @@ int main(int argc, char * argv[]) {
   char * pipes_directory = NULL;
 
   uint16_t operation = CLIENT_REQUEST_LIST_TASKS;
-  uint16_t opcode;
   uint16_t reptype;
-  uint32_t nbtasks;
-  uint32_t cmdArgc;
   uint64_t taskid;
   uint16_t errcode;
 
@@ -219,101 +360,15 @@ int main(int argc, char * argv[]) {
     write_op_to_pipe(operation, path_request, path_reply);
 
     //LECTURE
-    fd_reply = open(path_reply, O_RDONLY);
-    if (fd_reply == -1) { goto error; } 
-    //lecture du reptype
-    reptype = read_reptype(fd_reply, path_reply, path_request);
-    if (reptype != SERVER_REPLY_OK) { goto error; }
-    //lecture du nombre de tasks
-    err = read(fd_reply, &nbtasks, sizeof(uint32_t));
-    if (err == -1) { goto error; }
-    nbtasks = be32toh(nbtasks);
-    if (nbtasks != 0) {
-      //pour chaque task
-      for (int i = 0; i < nbtasks; i++) {
-        //lecture du taskid
-        err = read(fd_reply, &taskid, sizeof(uint64_t));
-        if (err == -1) { goto error; }
-        taskid = be64toh(taskid);
-        printf("%" PRId64 ": ", taskid);
-
-        //lecture du timing
-        timing *t = malloc(sizeof(timing));
-        if (t == NULL) { goto error; }
-        err = read(fd_reply, t, sizeof(uint64_t)+sizeof(uint32_t)+sizeof(uint8_t));
-        if (err == -1) { goto error; }
-        t->minutes = be64toh(t->minutes);
-        t->hours = be32toh(t->hours);
-        char *bufferTiming = malloc(TIMING_TEXT_MIN_BUFFERSIZE);
-        if (bufferTiming == NULL) { goto error; }
-        timing_string_from_timing(bufferTiming, t);
-        printf("%s ", bufferTiming);
-
-        //lecture de la commandline
-        err = read(fd_reply, &cmdArgc, sizeof(uint32_t)); //lecture du nombre d'argv pour la task[i]
-        if (err == -1) { goto error; }
-        cmdArgc = be32toh(cmdArgc);
-        for (int j = 0; j < cmdArgc; j++) {
-          uint32_t stringL;
-          err = read(fd_reply, &stringL, sizeof(uint32_t)); //lecture de la longueur de l'argv[i]
-          if (err == -1) { goto error; }
-          stringL = be32toh(stringL);
-          char bufferCmd[stringL+1];
-          err = read(fd_reply, bufferCmd, stringL); //lecture du contenu de l'argv[i]
-          if (err == -1) { goto error; }
-          bufferCmd[stringL] = '\0';
-          printf("%s ", bufferCmd);
-        }
-        free(t);
-        free(bufferTiming);
-        printf("\n");
-      }    
-    }
-    close(fd_reply);
+    read_list(path_reply, path_request);
   } //Fin commande LIST (-l)
 
 
   //Commande CREATE (-c)
   else if (operation == CLIENT_REQUEST_CREATE_TASK){
     //ECRITURE
-    fd_request = open(path_request, O_WRONLY);
-    if (fd_request == -1) { goto error; }
-    char buffer[1024];
-    
-    //ecriture de l'opcode
-    int buffer_size = sizeof(uint16_t);
-    opcode = htobe16(operation);
-    memmove(buffer,&opcode,sizeof(uint16_t));
-    
-    //ecriture du timing (MINUTES <uint64>, HOURS <uint32>, DAYSOFWEEK <uint8>)
-    struct timing t;
-    int n = timing_from_strings(&t, minutes_str, hours_str, daysofweek_str);
-    if (n==-1) { goto error; }
-    t.minutes = htobe64(t.minutes);
-    t.hours = htobe32(t.hours);
-    memmove(buffer+buffer_size, &t, sizeof(uint64_t)+sizeof(uint32_t)+sizeof(uint8_t));
-    buffer_size += sizeof(uint64_t)+sizeof(uint32_t)+sizeof(uint8_t);
-    
-    //ecriture de la commandline (ARGC <uint32>, ARGV[0] <string>, ..., ARGV[ARGC-1] <string>)
-    if(argc<1) goto error;
-    if(argv[0]==NULL) goto error;
-    //ecriture argc
-    cmdArgc = htobe32(argc-optind);
-    memmove(buffer+buffer_size, &cmdArgc, sizeof(uint32_t));
-    buffer_size += sizeof(uint32_t);
-    //ecriture argv
-    for (int i = optind; i < argc; i++) {
-      uint32_t length = strlen(argv[i]);
-      uint32_t h = htobe32(length);
-      memmove(buffer+buffer_size, &h, sizeof(uint32_t));
-      char *bufferCmd = argv[i];
-      memmove(buffer+buffer_size+sizeof(uint32_t), bufferCmd, length);
-      buffer_size += sizeof(uint32_t)+length;
-    }
-    err = write(fd_request, buffer, buffer_size);
-    if (err == -1) { goto error; }
-    close(fd_request);
-
+    write_create(operation, argc, argv, minutes_str, hours_str, daysofweek_str, path_request, path_reply);
+  
     //LECTURE
     fd_reply = open(path_reply, O_RDONLY);
     if (fd_reply == -1) { goto error; }
@@ -380,45 +435,7 @@ int main(int argc, char * argv[]) {
     write_op_taskid_to_pipe(operation, taskid, path_request, path_reply);
     
     //LECTURE
-    fd_reply = open(path_reply, O_RDONLY);
-    if (fd_reply == -1) { goto error; }
-    //lecture du reptype
-    reptype = read_reptype(fd_reply, path_reply, path_request);
-    //lecture pour reptype OK
-    if (reptype == SERVER_REPLY_OK) {
-      //lecture du nombre de runs
-      uint32_t nbruns;
-      err = read(fd_reply, &nbruns, sizeof(uint32_t));
-      if (err == -1) { goto error; }
-      nbruns = be32toh(nbruns);
-      for (int i=0; i<nbruns; i++) { //pour chaque run
-        //lecture de la date et heure du run
-        int64_t time;
-        err = read(fd_reply, &time, sizeof(int64_t));
-        if (err == -1) { goto error; }
-        time = be64toh((uint64_t)time);
-        struct tm * t = localtime(&time);
-        char strBuffer[80];
-        strftime(strBuffer, 80, "%F %T", t);
-        printf("%s ", strBuffer);
-        //lecture de l'exitcode du run
-        uint16_t exitcode;
-        err = read(fd_reply, &exitcode, sizeof(uint16_t));
-        if (err == -1) { goto error; }
-        exitcode = be16toh(exitcode);
-        printf("%u\n", (unsigned int)exitcode);
-      }
-    //lecture pour reptype ERROR
-    } else if (reptype == SERVER_REPLY_ERROR) {
-      //lecture de l'errcode
-      errcode = read_errcode(fd_reply, path_reply, path_request);
-      if (errcode == SERVER_REPLY_ERROR_NOT_FOUND) {
-        printf("il n'existe aucune tâche avec cet identifiant");
-        goto error;
-      }
-      
-    }
-    close(fd_reply);
+    read_times_exitcode(path_reply, path_request);
   } //Fin commande TIMES_EXITCODE (-x)
   
 
