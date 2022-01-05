ea78e67ef34ea3c8001107a85c2835a7eb02d3ba
Alass <alassane.paris@gmail.com>
2022-01-05 05:39:22 +0100

    démon créé

--- src/saturn.c
+++ src/saturn.c
@@ -1,38 +1,51 @@
 #include "../include/cassini.h"
 
+char *pipes_directory = "/tmp/<USERNAME>/saturnd/pipes";
 
 int create_daemon() {
     int pid = fork();
     if (pid == -1) {
-        //échec
         exit(1);
     } else if (pid == 0) {
-
-        //fils
-        printf("filspid %d\n",getpid());
-        sleep(1);
-        if(getppid()==1){
-            printf("daemon\n");
-            setsid(); //todo check erreur
+        sleep(1); //le temps que le père meure
+        if (getppid() == 1) {
+            printf("daemon crée \n");
+            int er = setsid(); //todo check erreur
+            if (er == -1) {
+                exit(1);
+            }
+            printf("PID : %d\nce terminal peut etre fermé\n", getpid());
             close(STDIN_FILENO);
             close(STDOUT_FILENO);
             close(STDERR_FILENO);
+            //nécessaire ?
             //todo log
-        }else{
-            printf("error %d\n",getppid());
+        } else {
+            printf("error %d\n", getppid());
         }
         return 0;
-    } else{
-
+    } else {
         //pere a tuer
-        printf("pere %d\n",getpid());
         exit(0);
         //envoyer un signal a catch avec le fils pour confirmer ?
     }
 }
 
+void read_from_pipes() {
+    while (1) { //boucle nécéssaire ?
+        int fd_request = open(pipes_directory, O_RDONLY);//test err
+        uint16_t request;
+        int rd = read(fd_request, &request, sizeof(uint16_t));//test err
+        request = be16toh(request);
+        switch (request) {
+            //TODO
+        }
+    }
+}
+
 int main() {
-    printf("print3 %d\n",create_daemon());
-    printf("\n%d\n",getppid());
+    create_daemon();
+    read_from_pipes();
+    printf("zzzz\n");
     exit(0);
 }
