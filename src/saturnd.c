1a8b0de78499f2528a0066d3606243b6ace49c31
Alass <alassane.paris@gmail.com>
2022-01-11 20:08:35 +0100

    ajouts des free pour completer terminate

--- src/saturnd.c
+++ src/saturnd.c
@@ -21,7 +21,7 @@ void create_daemon() {
 
 
 
-/*
+
 int send_reply_bool(int reptype){//0 pour ok et autre pour erreur
     int fd_reply = open(path_reply,O_WRONLY);
     if (fd_reply == -1) return 1;//todo
@@ -34,9 +34,9 @@ int send_reply_bool(int reptype){//0 pour ok et autre pour erreur
     close(fd_reply);
     return 0;
 }
- */
 
-/*
+
+
 void read_from_pipes() {
     int fd_request = open(path_request, O_RDONLY); //ou mettre le close ?
     struct pollfd pfd[1];
@@ -44,40 +44,40 @@ void read_from_pipes() {
     //pfd[0].events=POLLIN;
 
     while (1) { //boucle nécéssaire ?
-        poll(pfd,1,1000);
-        if (pfd[0].revents & POLLIN){
-        //test err
-        uint16_t request;
-        int rd = read(fd_request, &request, sizeof(uint16_t));
-        //test err
-        request = be16toh(request);
-        switch (request) {
-            case CLIENT_REQUEST_TERMINATE:
-                send_reply_bool(1);
-                goto exit_loop;
-            case CLIENT_REQUEST_LIST_TASKS:
-                list_tasks();
-                break;
-            case CLIENT_REQUEST_CREATE_TASK:
-                create_task(timing,commandline);
-                break;
-            case CLIENT_REQUEST_REMOVE_TASK:
-                remove_task(taskid);
-                break;
-            case CLIENT_REQUEST_GET_TIMES_AND_EXITCODES:
-                get_times_and_exitcodes(taskid);
-                break;
-            case CLIENT_REQUEST_GET_STDOUT:
-                get_stdout(taskid);
-                break;
-            case CLIENT_REQUEST_GET_STDERR:
-                get_stderr(taskid);
-                break;
+        poll(pfd, 1, 1000);
+        if (pfd[0].revents & POLLIN) {
+            //test err
+            uint16_t request;
+            int rd = read(fd_request, &request, sizeof(uint16_t));
+            //test err
+            request = be16toh(request);
+            switch (request) {
+                case CLIENT_REQUEST_TERMINATE:
+                    send_reply_bool(1);
+                    goto exit_loop;
+                case CLIENT_REQUEST_LIST_TASKS:
+                    list_tasks();
+                    break;
+                case CLIENT_REQUEST_CREATE_TASK:
+                    create_task(timing, commandline);
+                    break;
+                case CLIENT_REQUEST_REMOVE_TASK:
+                    remove_task(taskid);
+                    break;
+                case CLIENT_REQUEST_GET_TIMES_AND_EXITCODES:
+                    get_times_and_exitcodes(taskid);
+                    break;
+                case CLIENT_REQUEST_GET_STDOUT:
+                    get_stdout(taskid);
+                    break;
+                case CLIENT_REQUEST_GET_STDERR:
+                    get_stderr(taskid);
+                    break;
+            }
         }
+        exit_loop : //spaghetti ?
     }
-    exit_loop : //spaghetti ?
 }
-*/
 int main() {
     char *path_request = init_path_request(init_path());
     char *path_reply = init_path_reply(init_path());
@@ -85,8 +85,12 @@ int main() {
     if (mkfifo(path_reply,0666) == -1) { exit(EXIT_FAILURE); }
 
     create_daemon();
-    while (1){ }
-    //read_from_pipes();
-    printf("zzzz\n");
+    read_from_pipes();
+
+
+    free(path_request);
+    free(path_reply);
+    printf("zzzz\n"); //inutile
+
     exit(0);
 }
